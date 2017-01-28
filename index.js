850486dbbbfa83cb3e7dfe01dde72f9cb0f58341
Mateusz Wróbel <wrobel.mateusz@gmail.com>
2017-01-28 16:29:47 +0100

    add filenamePattern option to pass different file extensions

--- index.js
+++ index.js
@@ -4,23 +4,28 @@ var through = require('through');
 var filenamePattern = /\.(html|handlebars|hbs)$/;
 
 var wrap = function (template) {
-  return 'var templater = require("handlebars/runtime")["default"].template;' +
-         'module.exports = templater(' + template + ');'
+    return 'var templater = require("handlebars/runtime")["default"].template;' +
+        'module.exports = templater(' + template + ');'
 }
 
-module.exports = function (file) {
-  if (!filenamePattern.test(file)) return through();
+module.exports = function (file, options) {
+    if (!options.filenamePattern) {
+        options.filenamePattern = filenamePattern;
+    }
 
-  var input = '';
-  var write = function (buffer) {
-    input += buffer;
-  }
+    var regexp = new RegExp(options.filnamePattern);
+    if (!regexp.test(file)) return through();
 
-  var end = function () {
-    this.queue(wrap(handlebars.precompile(input)));
-    this.queue(null);
-  }
+    var input = '';
+    var write = function (buffer) {
+        input += buffer;
+    };
 
-  return through(write, end);
+    var end = function () {
+        this.queue(wrap(handlebars.precompile(input)));
+        this.queue(null);
+    };
 
-}
+    return through(write, end);
+
+};
